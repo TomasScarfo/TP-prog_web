88d8eedbfcfb31c4ca349fda3e26a8082ca6c51b
tomas scarfo <tomas.scarfo@gmail.com>
2022-10-17 16:41:20 -0300

    lala

--- tp.js
+++ tp.js
@@ -7,24 +7,24 @@ const estado = {
 }
 
 function experiencia() {
-    var fuel = document.getElementById("animo-input").value
+    var a = document.getElementById("animo-input").value
     
     var imgPath = null
  
-    if (10 <= fuel && fuel < 8) {
+    if (10 <= a && a < 8) {
         imgPath = estado.excelente
-    } else if (8 <= fuel && fuel < 6) {
+    } else if (8 <= a && a < 6) {
         imgPath = estado.buena
-    } else if (6 <= fuel && fuel < 4) {
+    } else if (6 <= a && a < 4) {
         imgPath = estado.intermedio
-    } else if (4 <= fuel && fuel < 2) {
+    } else if (4 <= a && a < 2) {
         imgPath = estado.mala
-    } else if (fuel >= 0 && fuel <= 2) {
+    } else if (a >= 0 && a <= 2) {
         imgPath = estado.muyMala
     } else {
         prompt('valor invalido, ingrese un numero entre 1-10')
     }
 
-    var medidor = document.getElementById('animo-img')
-    medidor.src = imgPath
+    var cara = document.getElementById('animo-img')
+    cara.src = imgPath
 }
